1c40994934e81677631ea95cbb1cfb1dfb218491
Christopher Kiessling <Crsk@mail.com>
2022-09-30 00:07:59 -0300

    🔨 Implement redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,21 @@ import AuthContextProvider from './contexts/AuthContext'
 import Login from './pages/login'
 import { themeChange } from 'theme-change'
 import { useEffect } from 'react'
-
+import { useAppDispatch, useAppSelector } from './app/hooks'
+import { increment, decrement } from './features/counter/counterSlice'
 
 function App() {
+  const count = useAppSelector(state => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  function handleIncrement() {
+    dispatch(increment())
+  }
+
+  function handleAddAmount() {
+    dispatch(decrement())
+  }
+
   useEffect(() => {
     themeChange(false)
   }, [])
@@ -19,6 +31,13 @@ function App() {
       <option value="mytheme">mytheme</option>
     </select> */}
     <Login />
+    <div className="btn btn-accent" onClick={handleAddAmount}>
+      Decrement {count}
+    </div>
+    <div className="btn btn-primary" onClick={handleIncrement}>
+      Increment {count}
+    </div>
+
   </AuthContextProvider>
 }
 
