0631d42c52eccf4dc21f6d36b284d9b599826b03
Christopher Kiessling <Crsk@mail.com>
2022-11-14 17:51:19 +0100

    💄 Style product card selection

--- src/components/Product.component.tsx
+++ src/components/Product.component.tsx
@@ -14,25 +14,27 @@ export default function Product(props: { product: ProductWithRefs }) {
     setProducts([...products])
   }
 
-  return !product.name ? <></> : <div key={product.id} >
-    {product.photo
-      ? <figure><img className='w-full' src={product.photo} /></figure>
-      : <></>
-    }
-    <div className="card-body">
-      <h2 className="card-title text-[#734832]">{product.name}</h2>
-      {product.activeOption === 'DESCRIPTION' ? <p className="text-[#734832]">{product.description}</p> :
-        <ul className="list-disc text-[#734832]">
-          {product.ingredients?.map(ingredient => <li key={ingredient.id}>{ingredient.name}</li>)}
-        </ul>
-      } {!product.ingredientIds?.length
-        ? <></> /** Hide 'Ingredients' button if none */
-        : <div className="card-actions justify-end">
-          <button className={product.activeOption === 'DESCRIPTION' ? 'btn btn-secondary' : 'btn btn-primary'} onClick={() => _toggleIngredients(product)}>
-            Ingredientes
-          </button>
-        </div>
+  return !product.name
+    ? <></>
+    : <div key={product.id} className={`${product.activeOption !== 'DESCRIPTION' ? 'rounded-[18px] border-2 border-[#bf7b5a]' : 'rounded-[16px] border-0'}`}>
+      {product.photo
+        ? <figure><img className='w-full rounded-t-[16px]' src={product.photo} /></figure>
+        : <></>
       }
+      <div className="card-body">
+        <h2 className="card-title text-[#734832]">{product.name}</h2>
+        {product.activeOption === 'DESCRIPTION' ? <p className="text-[#734832]">{product.description}</p> :
+          <ul className="list-disc text-[#734832]">
+            {product.ingredients?.map(ingredient => <li key={ingredient.id}>{ingredient.name}</li>)}
+          </ul>
+        } {!product.ingredientIds?.length
+          ? <></> /** Hide 'Ingredients' button if none */
+          : <div className="card-actions justify-end">
+            <button className={product.activeOption === 'DESCRIPTION' ? 'btn btn-secondary' : 'btn btn-primary'} onClick={() => _toggleIngredients(product)}>
+              Ingredientes
+            </button>
+          </div>
+        }
+      </div>
     </div>
-  </div>
 }
